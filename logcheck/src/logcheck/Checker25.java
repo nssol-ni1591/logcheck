8270a1c1dd9766f27a63388cad4a57c46d0a0b56
gohdo <gohdo.kou.2c3@jp.nssol.nssmc.com>
2017-11-24 09:55:03 +0900

    Checker24欠番に伴いChecker25の継承を解除

--- logcheck/src/logcheck/Checker25.java
+++ logcheck/src/logcheck/Checker25.java
@@ -1,7 +1,12 @@
 package logcheck;
 
+import java.io.PrintWriter;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Optional;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import javax.inject.Inject;
@@ -9,19 +14,34 @@ import javax.inject.Inject;
 import org.jboss.weld.environment.se.Weld;
 import org.jboss.weld.environment.se.WeldContainer;
 
+import logcheck.isp.IspList;
+import logcheck.known.KnownList;
+import logcheck.log.AccessLog;
 import logcheck.log.AccessLogBean;
+import logcheck.log.AccessLogSummary;
+import logcheck.mag.MagList;
+import logcheck.util.net.NetAddr;
 
 /*
  * ログ解析用の集約ツール1：
  * 国 > ISP > クライアントIP > メッセージ  > ID 毎にログ数を集計する。
  * 利用方法としては、プログラムの出力を直接参照するのではなく、Excelに読み込ませpivotで解析する想定のためTSV形式で出力する。
- * なお、このツールでは、正常系ログは集約を行う。
+ * なお、このツールでは、異常系ログは個々のメッセージ単位で出力。正常系ログは集約を行う。
  */
-public class Checker25 extends Checker24 {
+//public class Checker25 extends Checker24 {
+public class Checker25 extends AbstractChecker<List<AccessLogSummary>> {
+
+	@Inject protected KnownList knownlist;
+	@Inject protected MagList maglist;
 
 	@Inject private Logger log;
 
-	@Override
+	public Checker25 init(String knownfile, String magfile) throws Exception {
+		this.knownlist.load(knownfile);
+		this.maglist.load(magfile);
+		return this;
+	}
+
 	protected String getPattern(AccessLogBean b) {
 		Optional<String> rc = Stream.of(FAIL_PATTERNS)
 				.filter(p -> p.matcher(b.getMsg()).matches())
@@ -49,6 +69,56 @@ public class Checker25 extends Checker24 {
 		}
 		log.warning("(Pattern): \"" + b.getMsg() + "\"");
 		return "<Warn>" + b.getMsg();
+	}
+
+	@Override
+	public List<AccessLogSummary> call(Stream<String> stream)
+			throws Exception {
+		final List<AccessLogSummary> list = Collections.synchronizedList(new LinkedList<>());;
+		stream//.parallel()
+				.filter(AccessLog::test)
+				.map(AccessLog::parse)
+				.forEach(b -> {
+					String pattern = getPattern(b);
+					NetAddr addr = b.getAddr();
+					IspList isp = maglist.get(addr);
+					if (isp == null) {
+						isp = knownlist.get(addr);
+					}
+
+					if (isp != null) {
+						AccessLogSummary msg;
+
+//						if (INFO_SUMMARY_MSG.equals(pattern)) {
+//						}
+//						else {
+							msg = new AccessLogSummary(b, pattern, isp);
+							list.add(msg);
+//						}
+					}
+					else {
+						addrErrs.add(b.getAddr());
+					}
+				});
+		return list;
+	}
+
+	@Override
+	public void report(final PrintWriter out, final List<AccessLogSummary> list) {
+		out.println("発生日時\t発生日\t国\tISP/プロジェクト\tアドレス\tユーザID\tメッセージ\tロール");
+		list.forEach(msg -> 
+			out.println(Stream.of(msg.getFirstDate()
+					, msg.getFirstDate().substring(0, 10)
+					, msg.getIsp().getCountry()
+					, msg.getIsp().getName()
+					, msg.getAddr().toString()
+					, msg.getId()
+					, msg.getPattern()
+					, String.join(",", msg.getRoles())
+					)
+					.collect(Collectors.joining("\t"))
+					)
+				);
 	}
 
 	public static void main(String... argv) {
--- logcheck/test/logcheck/test/MainTest.java
+++ logcheck/test/logcheck/test/MainTest.java
@@ -78,189 +78,139 @@ public class MainTest {
 			application.init(KNOWNLIST, MAGLIST).start(new String[] { ACCESSLOG }, 0);
 		}
 		catch (Exception ex) {
-//			ex.printStackTrace(System.err);
 		}
 	}
 	@Test
 	public void test8() {
-//		logcheck.Checker8.main(KNOWNLIST, MAGLIST, ACCESSLOG);
 		Weld weld = new Weld();
 		try (WeldContainer container = weld.initialize()) {
-//			Checker8 application = container.instance().select(Checker8.class, new AnnotationLiteral<UseChecker8>(){
 			Checker8 application = container.select(Checker8.class, new AnnotationLiteral<UseChecker8>(){
 				private static final long serialVersionUID = 1L;
 			}).get();
 			application.init(KNOWNLIST, MAGLIST).start(new String[] { ACCESSLOG }, 0);
 		}
 		catch (Exception ex) {
-//			ex.printStackTrace(System.err);
 		}
 	}
 	@Test
 	public void test9() {
-//		logcheck.Checker9.main("2017-01-30", KNOWNLIST, MAGLIST, ACCESSLOG);
 		Weld weld = new Weld();
 		try (WeldContainer container = weld.initialize()) {
-//			Checker9 application = container.instance().select(Checker9.class).get();
 			Checker9 application = container.select(Checker9.class).get();
 			application.init(KNOWNLIST, MAGLIST, "2017-01-30").start(new String[] { ACCESSLOG }, 0);
 		}
 		catch (Exception ex) {
-//			ex.printStackTrace(System.err);
 		}
 	}
 
 	@Test
 	public void test10() {
-//		logcheck.Checker10.main(KNOWNLIST, MAGLIST, ACCESSLOG);
 		Weld weld = new Weld();
 		try (WeldContainer container = weld.initialize()) {
-//			Checker10 application = container.instance().select(Checker10.class).get();
 			Checker10 application = container.select(Checker10.class).get();
 			application.init(KNOWNLIST, MAGLIST).start(new String[] { ACCESSLOG }, 0);
 		}
 		catch (Exception ex) {
-//			ex.printStackTrace(System.err);
 		}
 	}
 
 	@Test
 	public void test12() {
-//		logcheck.Checker12.main(KNOWNLIST, MAGLIST, ACCESSLOG);
 		Weld weld = new Weld();
 		try (WeldContainer container = weld.initialize()) {
-//			Checker12 application = container.instance().select(Checker12.class).get();
 			Checker12 application = container.select(Checker12.class).get();
 			application.init(KNOWNLIST, MAGLIST).start(new String[] { ACCESSLOG }, 0);
 		}
 		catch (Exception ex) {
-//			ex.printStackTrace(System.err);
 		}
 	}
 	@Test
 	public void test13() {
-//		logcheck.Checker13.main(KNOWNLIST, MAGLIST, ACCESSLOG);
 		Weld weld = new Weld();
 		try (WeldContainer container = weld.initialize()) {
-//			Checker13 application = container.instance().select(Checker13.class).get();
 			Checker13 application = container.select(Checker13.class).get();
 			application.init(KNOWNLIST, MAGLIST).start(new String[] { ACCESSLOG }, 0);
 		}
 		catch (Exception ex) {
-//			ex.printStackTrace(System.err);
 		}
 	}
 	@Test
 	public void test14() {
-//		logcheck.Checker14.main(KNOWNLIST, MAGLIST, ACCESSLOG);
 		Weld weld = new Weld();
 		try (WeldContainer container = weld.initialize()) {
-//			Checker14 application = container.instance().select(Checker14.class, new AnnotationLiteral<UseChecker14>(){
 			Checker14 application = container.select(Checker14.class, new AnnotationLiteral<UseChecker14>(){
 				private static final long serialVersionUID = 1L;
 			}).get();
 			application.init(KNOWNLIST, SSLINDEX).start(new String[] { ACCESSLOG }, 0);
 		}
 		catch (Exception ex) {
-//			ex.printStackTrace(System.err);
 		}
 	}
 	@Test
 	public void test15() {
-//		logcheck.Checker15.main(KNOWNLIST, MAGLIST, ACCESSLOG);
 		Weld weld = new Weld();
 		try (WeldContainer container = weld.initialize()) {
-//			Checker15 application = container.instance().select(Checker15.class).get();
 			Checker15 application = container.select(Checker15.class).get();
 			application.init(KNOWNLIST, SSLINDEX).start(new String[] { ACCESSLOG }, 0);
 		}
 		catch (Exception ex) {
-//			ex.printStackTrace(System.err);
 		}
 	}
 	@Test
 	public void test16() {
-//		logcheck.Checker16.main(KNOWNLIST, MAGLIST, ACCESSLOG);
 		Weld weld = new Weld();
 		try (WeldContainer container = weld.initialize()) {
-//			Checker16 application = container.instance().select(Checker16.class).get();
 			Checker16 application = container.select(Checker16.class).get();
 			application.init(KNOWNLIST, SSLINDEX).start(new String[] { ACCESSLOG }, 0);
 		}
 		catch (Exception ex) {
-//			ex.printStackTrace(System.err);
 		}
 	}
 	@Test
 	public void test17() {
-//		logcheck.Checker17.main(KNOWNLIST, MAGLIST, ACCESSLOG);
 		Weld weld = new Weld();
 		try (WeldContainer container = weld.initialize()) {
-//			Checker17 application = container.instance().select(Checker17.class).get();
 			Checker17 application = container.select(Checker17.class).get();
 			application.init(KNOWNLIST, SSLINDEX).start(new String[] { ACCESSLOG }, 0);
 		}
 		catch (Exception ex) {
-//			ex.printStackTrace(System.err);
 		}
 	}
 	@Test
 	public void test18() {
-//		logcheck.Checker18.main(KNOWNLIST, MAGLIST, ACCESSLOG);
 		Weld weld = new Weld();
 		try (WeldContainer container = weld.initialize()) {
-//			Checker18 application = container.instance().select(Checker18.class).get();
 			Checker18 application = container.select(Checker18.class).get();
 			application.init(KNOWNLIST, SSLINDEX).start(new String[] { ACCESSLOG }, 0);
 		}
 		catch (Exception ex) {
-//			ex.printStackTrace(System.err);
 		}
 	}
 	@Test
 	public void test22() {
-//		logcheck.Checker22.main(KNOWNLIST, MAGLIST, ACCESSLOG);
 		Weld weld = new Weld();
 		try (WeldContainer container = weld.initialize()) {
-//			Checker22 application = container.instance().select(Checker22.class).get();
 			Checker22 application = container.select(Checker22.class).get();
 			application.init(KNOWNLIST, MAGLIST).start(new String[] { ACCESSLOG }, 0);
 		}
 		catch (Exception ex) {
-//			ex.printStackTrace(System.err);
 		}
 	}
 	@Test
 	public void test23() {
-//		logcheck.Checker23.main(KNOWNLIST, MAGLIST, ACCESSLOG);
 		Weld weld = new Weld();
 		try (WeldContainer container = weld.initialize()) {
-//			Checker23 application = container.instance().select(Checker23.class).get();
 			Checker23 application = container.select(Checker23.class).get();
 			application.init(KNOWNLIST, MAGLIST).start(new String[] { ACCESSLOG }, 0);
 		}
 		catch (Exception ex) {
-//			ex.printStackTrace(System.err);
-		}
-	}
-	@Test
-	public void test24() {
-//		logcheck.Checker23.main(KNOWNLIST, MAGLIST, ACCESSLOG);
-		Weld weld = new Weld();
-		try (WeldContainer container = weld.initialize()) {
-//			Checker23 application = container.instance().select(Checker23.class).get();
-			Checker24 application = container.select(Checker24.class).get();
-			application.init(KNOWNLIST, MAGLIST).start(new String[] { ACCESSLOG }, 0);
-		}
-		catch (Exception ex) {
-//			ex.printStackTrace(System.err);
 		}
 	}
 	@Test
 	public void test25() {
 		Weld weld = new Weld();
 		try (WeldContainer container = weld.initialize()) {
-			Checker24 application = container.select(Checker25.class).get();
+			Checker25 application = container.select(Checker25.class).get();
 			application.init(KNOWNLIST, MAGLIST).start(new String[] { ACCESSLOG }, 0);
 		}
 		catch (Exception ex) {
