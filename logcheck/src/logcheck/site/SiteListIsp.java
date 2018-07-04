7e7e830533816cba6580ec290d188efd489c09c6
gohdo <gohdo.kou.2c3@jp.nssol.nssmc.com>
2018-07-04 10:33:45 +0900

    使われ方が不明確なメソッドの配置換え

--- logcheck/src/logcheck/site/SiteListIsp.java
+++ logcheck/src/logcheck/site/SiteListIsp.java
@@ -4,11 +4,18 @@ import logcheck.isp.IspList;
 
 public interface SiteListIsp extends IspList {
 
-	String getSiteId();
-
 	String getSiteName();
 	String getSiteDelFlag();
 	String getProjId();
 	String getProjDelFlag();
+	
+	default String getName() {
+		// 利用申請ファイル(Excel)には該当する属性は存在しないため
+		throw new IllegalArgumentException("not impliment");
+	}
+	default String getSiteId() {
+		// 利用申請ファイル(Excel)には該当する属性は存在しないため
+		throw new IllegalArgumentException("not impliment");
+	}
 
 }
