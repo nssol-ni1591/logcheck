9631c9a22e5e7eb3bfdb09402c157dceb7cdd354
gohdo <gohdo.kou.2c3@jp.nssol.nssmc.com>
2018-08-23 15:20:12 +0900

    文字列連結の速度試験

--- logcheck/src/logcheck/Checker14.java
+++ logcheck/src/logcheck/Checker14.java
@@ -2,6 +2,7 @@ package logcheck;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.sql.SQLException;
 import java.text.Normalizer;
 import java.util.Objects;
@@ -13,6 +14,7 @@ import javax.enterprise.util.AnnotationLiteral;
 import javax.inject.Inject;
 
 import logcheck.annotations.UseChecker14;
+import logcheck.annotations.WithElaps;
 import logcheck.known.KnownList;
 import logcheck.known.KnownListIsp;
 import logcheck.log.AccessLog;
@@ -119,8 +121,8 @@ public class Checker14 extends AbstractChecker<UserList<UserListBean>> {
 		return userlist;
 	}
 
-	@Override
-	public void report(final PrintWriter out, final UserList<UserListBean> list) {
+	@WithElaps
+	private void report1(final PrintWriter out, final UserList<UserListBean> list) {
 		// アドレスを出力してはいけない。拠点ごとに回数を取得しているのに、アドレスを出力すると、回数は実際の値のアドレス数の倍になる
 		out.println("ユーザID\t国\tISP/プロジェクトID\t拠点名\tプロジェクト削除\t拠点削除\tユーザ削除\t有効\t初回日時\t最終日時\t接続回数\t失効日時\tユーザ回数");
 		userlist.values().stream()
@@ -162,6 +164,177 @@ public class Checker14 extends AbstractChecker<UserList<UserListBean>> {
 							);
 				}
 			});
+	}
+	@WithElaps
+	private void report2(final PrintWriter out, final UserList<UserListBean> list) {
+		// アドレスを出力してはいけない。拠点ごとに回数を取得しているのに、アドレスを出力すると、回数は実際の値のアドレス数の倍になる
+		out.println("ユーザID\t国\tISP/プロジェクトID\t拠点名\tプロジェクト削除\t拠点削除\tユーザ削除\t有効\t初回日時\t最終日時\t接続回数\t失効日時\tユーザ回数");
+		userlist.values().stream()
+			.forEach(user -> {
+				if (user.getSites().isEmpty()) {
+					out.println(String.join("\t"
+							, user.getUserId()
+							, "-"
+							, "-"
+							, "-"
+							, "-1"
+							, "-1"
+							, "-1"
+							, user.getValidFlag()
+							, ""
+							, ""
+							, "0"
+							, user.getRevoce()
+							,"0"
+							));
+				}
+				else {
+					user.getSites().forEach(site ->
+						out.println(String.join("\t"
+								, user.getUserId()
+								, site.getCountry()
+								, site.getProjId()
+								, site.getSiteName()
+								, site.getProjDelFlag()
+								, site.getSiteDelFlag()
+								, site.getUserDelFlag()
+								, user.getValidFlag()
+								, site.getFirstDate()
+								, site.getLastDate()
+								, String.valueOf(site.getCount())
+								, user.getRevoce()
+								, String.valueOf(user.getTotal())
+								))
+							);
+				}
+			});
+	}
+	@WithElaps
+	private void report3(final PrintWriter out, final UserList<UserListBean> list) {
+		// アドレスを出力してはいけない。拠点ごとに回数を取得しているのに、アドレスを出力すると、回数は実際の値のアドレス数の倍になる
+		out.println("ユーザID\t国\tISP/プロジェクトID\t拠点名\tプロジェクト削除\t拠点削除\tユーザ削除\t有効\t初回日時\t最終日時\t接続回数\t失効日時\tユーザ回数");
+		userlist.values().stream()
+			.forEach(user -> {
+				if (user.getSites().isEmpty()) {
+					out.println(user.getUserId()
+							+ "\t" +  "-"
+							+ "\t" +  "-"
+							+ "\t" +  "-"
+							+ "\t" +  "-1"
+							+ "\t" +  "-1"
+							+ "\t" +  "-1"
+							+ "\t" +  user.getValidFlag()
+							+ "\t" +  ""
+							+ "\t" +  ""
+							+ "\t" +  "0"
+							+ "\t" +  user.getRevoce()
+							+ "\t" + "0"
+							);
+				}
+				else {
+					user.getSites().forEach(site ->
+						out.println(user.getUserId()
+								+ "\t" +  site.getCountry()
+								+ "\t" +  site.getProjId()
+								+ "\t" +  site.getSiteName()
+								+ "\t" +  site.getProjDelFlag()
+								+ "\t" +  site.getSiteDelFlag()
+								+ "\t" +  site.getUserDelFlag()
+								+ "\t" +  user.getValidFlag()
+								+ "\t" +  site.getFirstDate()
+								+ "\t" +  site.getLastDate()
+								+ "\t" +  site.getCount()
+								+ "\t" +  user.getRevoce()
+								+ "\t" +  user.getTotal()
+								)
+							);
+				}
+			});
+	}
+	@WithElaps
+	private void report4(final PrintWriter out, final UserList<UserListBean> list) {
+		// アドレスを出力してはいけない。拠点ごとに回数を取得しているのに、アドレスを出力すると、回数は実際の値のアドレス数の倍になる
+		out.println("ユーザID\t国\tISP/プロジェクトID\t拠点名\tプロジェクト削除\t拠点削除\tユーザ削除\t有効\t初回日時\t最終日時\t接続回数\t失効日時\tユーザ回数");
+		userlist.values().stream()
+			.forEach(user -> {
+				if (user.getSites().isEmpty()) {
+					out.println(new StringBuilder()
+							.append("\t").append(user.getUserId())
+							.append("\t").append("-")
+							.append("\t").append("-")
+							.append("\t").append("-")
+							.append("\t").append("-1")
+							.append("\t").append("-1")
+							.append("\t").append("-1")
+							.append("\t").append(user.getValidFlag())
+							.append("\t").append("")
+							.append("\t").append("")
+							.append("\t").append("0")
+							.append("\t").append(user.getRevoce())
+							.append("\t").append("0")
+							);
+				}
+				else {
+					user.getSites().forEach(site ->
+						out.println(new StringBuilder()
+								.append("\t").append(user.getUserId())
+								.append("\t").append(site.getCountry())
+								.append("\t").append(site.getProjId())
+								.append("\t").append(site.getSiteName())
+								.append("\t").append(site.getProjDelFlag())
+								.append("\t").append(site.getSiteDelFlag())
+								.append("\t").append(site.getUserDelFlag())
+								.append("\t").append(user.getValidFlag())
+								.append("\t").append(site.getFirstDate())
+								.append("\t").append(site.getLastDate())
+								.append("\t").append(site.getCount())
+								.append("\t").append(user.getRevoce())
+								.append("\t").append(user.getTotal())
+								)
+							);
+				}
+			});
+	}
+	@Override
+	public void report(final PrintWriter out, final UserList<UserListBean> list) {
+		long elaps;
+		PrintWriter nul;
+
+		nul = new PrintWriter(new StringWriter(100000));
+		elaps = System.currentTimeMillis();
+		report2(out, list);
+		System.err.println("dummy__: elapse=" + (System.currentTimeMillis() - elaps) + "ms");
+		nul.close();
+
+		nul = new PrintWriter(new StringWriter(100000));
+		elaps = System.currentTimeMillis();
+		report1(nul, list);
+		System.err.println("report1: elapse=" + (System.currentTimeMillis() - elaps) + "ms");
+		nul.close();
+
+		nul = new PrintWriter(new StringWriter(100000));
+		elaps = System.currentTimeMillis();
+		report1(nul, list);
+		System.err.println("report1: elapse=" + (System.currentTimeMillis() - elaps) + "ms");
+		nul.close();
+
+		nul = new PrintWriter(new StringWriter(100000));
+		elaps = System.currentTimeMillis();
+		report2(nul, list);
+		System.err.println("report2: elapse=" + (System.currentTimeMillis() - elaps) + "ms");
+		nul.close();
+
+		nul = new PrintWriter(new StringWriter(100000));
+		elaps = System.currentTimeMillis();
+		report3(nul, list);
+		System.err.println("report3: elapse=" + (System.currentTimeMillis() - elaps) + "ms");
+		nul.close();
+
+		nul = new PrintWriter(new StringWriter(100000));
+		elaps = System.currentTimeMillis();
+		report4(nul, list);
+		System.err.println("report4: elapse=" + (System.currentTimeMillis() - elaps) + "ms");
+		nul.close();
 	}
 
 	@Override
